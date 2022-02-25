920dfd8946628b21a78f33996a9fb898e2ccc0d9
Scott <burtonsb@gmail.com>
2022-02-24 19:22:39 -0500

    added restauraunt layout

---
+++ restaurant_layout/js/main.js
@@ -0,0 +1,20 @@
+
+
+const name = 'John';
+const age = 30
+const isCool = true;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof age);
+
+console.log('hello my name is');
+
+
+// for(let i = 0; i <= 10; i++) {
+//     console.log("For Loop Number: ${i}");
+//     console.log("for loop number" + i);
+// }
+
